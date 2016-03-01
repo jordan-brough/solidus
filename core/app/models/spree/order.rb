e941f3da853b677fb5fd9f062ac42626c6f6688d
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2016-02-29 16:45:53 -0800

    Quick return out of add_store_credit_payment
    If we don't have a user, we don't need to be inside of
    add_store_credit_payment.
    
    If we have a user that doesn't have any credit, and our order doesn't
    have any credit, we don't need to be inside of add_store_credit_payment.
    
    To accomplish this we need to *change existing behavior* of
    add_store_credit_payment to not add the error message given the previous
    changes. I argue its previous implementation was overstepping its bounds
    in assuming it should know when and how to add that error onto the
    order.
    
    Related to #908

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -623,6 +623,9 @@ module Spree
     end
 
     def add_store_credit_payments
+      return if user.nil?
+      return if payments.store_credits.checkout.empty? && user.total_available_store_credit.zero?
+
       payments.store_credits.checkout.each(&:invalidate!)
 
       # this can happen when multiple payments are present, auto_capture is
@@ -632,7 +635,7 @@ module Spree
 
       remaining_total = outstanding_balance - authorized_total
 
-      if user && user.store_credits.any?
+      if user.store_credits.any?
         payment_method = Spree::PaymentMethod::StoreCredit.first
 
         user.store_credits.order_by_priority.each do |credit|
