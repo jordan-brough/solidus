6f8ef4043692c975fbb5a7f92080cdda13ecd359
Vlad Stoica <vlad96stoica@gmail.com>
2017-04-21 11:36:21 +0100

    Temporarily disable specs that create TaxRates https://github.com/solidusio/solidus/pull/1851 will change the type of relation to a many to many so specs will temporarily need to be skipped until changes are made to the specs.

--- core/spec/models/spree/tax/item_adjuster_spec.rb
+++ core/spec/models/spree/tax/item_adjuster_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe Spree::Tax::ItemAdjuster do
+RSpec.describe Spree::Tax::ItemAdjuster, skip: true do
   subject(:adjuster) { described_class.new(item) }
   let(:order) { create(:order) }
   let(:item) { create(:line_item, order: order) }
@@ -65,16 +65,44 @@ RSpec.describe Spree::Tax::ItemAdjuster do
 
       context 'when there are matching rates for the zone' do
         context 'and all rates have the same tax category as the item' do
-          let(:item_tax_category) { build_stubbed(:tax_category) }
-          let(:rate_1) { create :tax_rate, tax_category: item_tax_category }
+          let(:item) { create :line_item, order: order, tax_category: item_tax_category }
+          let(:item_tax_category) { create(:tax_category) }
+          let(:rate_1) { create :tax_rate, tax_categories: [item_tax_category], amount: 0.1 }
           let(:rate_2) { create :tax_rate }
-          let(:rates_for_order_zone) { [rate_1, rate_2] }
+          let(:rate_3) { create :tax_rate, tax_categories: [item_tax_category, build(:tax_category)] }
+          let(:rates_for_order_zone) { [rate_1, rate_2, rate_3] }
 
           before { allow(item).to receive(:tax_category).and_return(item_tax_category) }
 
           it 'creates an adjustment for every matching rate' do
             adjuster.adjust!
-            expect(tax_adjustments.length).to eq(1)
+            expect(tax_adjustments.length).to eq(2)
+          end
+
+          it 'creates adjustments only for matching rates' do
+            adjuster.adjust!
+            expect(tax_adjustments.map(&:source)).to match_array([rate_1, rate_3])
+          end
+
+          context 'when the adjustment exists' do
+            before do
+              adjuster.adjust!
+            end
+
+            context 'when the existing adjustment is finalized' do
+              before do
+                tax_adjustments.first.finalize!
+              end
+
+              it 'updates the adjustment' do
+                item.update_columns(price: item.price * 2)
+                adjuster.adjust!
+                tax_rate1_adjustment = tax_adjustments.detect do |adjustment|
+                  adjustment.source == rate_1
+                end
+                expect(tax_rate1_adjustment.amount).to eq(0.1 * item.price)
+              end
+            end
           end
         end
       end
