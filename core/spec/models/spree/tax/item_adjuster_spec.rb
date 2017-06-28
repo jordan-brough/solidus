e14d7c4e18b3cd4d3213fdb51c07c6b6a58d175e
Vlad Stoica <vlad96stoica@gmail.com>
2017-06-28 10:40:25 +0100

    Update spec to reflect two tax categories

--- core/spec/models/spree/tax/item_adjuster_spec.rb
+++ core/spec/models/spree/tax/item_adjuster_spec.rb
@@ -118,7 +118,7 @@ RSpec.describe Spree::Tax::ItemAdjuster do
               it 'updates the adjustment' do
                 item.update_columns(price: item.price * 2)
                 adjuster.adjust!
-                expect(tax_adjustments.length).to eq(1)
+                expect(tax_adjustments.length).to eq(2)
                 expect(tax_adjustments.first.amount).to eq(0.1 * item.price)
               end
             end
